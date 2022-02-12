1baf85639ce69d3cb8b3d75f79957305995d8301
Rajesh Kumar <rajesh.k@avanzasolutions.com>
2022-02-12 21:29:06 +0500

    Initial Commit

---
+++ SwipeButton/SwipeButton.h
@@ -0,0 +1,18 @@
+//
+//  SwipeButton.h
+//  SwipeButton
+//
+//  Created by Rajesh Kumar Sahil on 12/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwipeButton.
+FOUNDATION_EXPORT double SwipeButtonVersionNumber;
+
+//! Project version string for SwipeButton.
+FOUNDATION_EXPORT const unsigned char SwipeButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeButton/PublicHeader.h>
+
+
